dacc784045166129d81da8589f3dcd2bd1effc66
Oh Dahyun <odh550@kaoni.com>
2024-09-10 19:48:51 +0900

    이미지 넣는 법

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,5 +1,6 @@
+import { Button, Navbar, Container, Nav, Row, Col } from 'react-bootstrap';
 import './App.css';
-import { Button, Navbar, Container, Nav } from 'react-bootstrap';
+import getImg from './img/woman.jpg';
 
 function App() {
     return (
@@ -7,14 +8,35 @@ function App() {
             {/*<Button variant="primary">Primary</Button>*/}
             <Navbar bg="dark" data-bs-theme="dark">
                 <Container>
-                    <Navbar.Brand href="#home">Dapui</Navbar.Brand>
+                    <Navbar.Brand href="#home">Dapui Shop</Navbar.Brand>
                     <Nav className="me-auto">
                         <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        <Nav.Link href="#features">New</Nav.Link>
+                        <Nav.Link href="#pricing">Best</Nav.Link>
                     </Nav>
                 </Container>
             </Navbar>
+
+            <div className="main-bg" style={{backgroundImage : 'url(' + getImg + ')'}}></div>
+            <Container>
+                <Row>
+                    <Col>
+                        <img src={process.env.PUBLIC_URL + '/logo192.png'} width="80%"/>
+                        <h4>상품명</h4>
+                        <p>상품설명</p>
+                    </Col>
+                    <Col>
+                        <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%"/>
+                        <h4>상품명</h4>
+                        <p>상품설명</p>
+                    </Col>
+                    <Col>
+                        <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%"/>
+                        <h4>상품명</h4>
+                        <p>상품설명</p>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
